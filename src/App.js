cd09c73ec89f004ae263fa01e09dc0c7de5e1cbd
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-08-16 22:03:37 +0530

    feat/adding TimerList and ToggleableTimerForm class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
 
 export default class TimersDashboard extends React.Component {
 	render() {
 		return(
 			<div className="max-w-sm mx-auto mt-8 border border-black border-solid rounded-lg">
 				<h2 className="mx-auto mt-4 text-3xl max-w-max">Timers</h2>
+				<TimerList />
+				<ToggleableTimerForm isOpen={true}/>
 			</div>
 		)
+	}
+}
+
+class TimerList extends React.Component {
+	render() {
+		return (
+			<div>
+				<EditTimer 
+					title="Guitar"
+					project="Music"
+					elapsed="194856"
+					runningSince="0000"
+					editForOpen={false}
+				/>	
+				<EditTimer 
+					title="Swimming"
+					project="Olympic"
+					elapsed="294856"
+					runningSince="0054"
+					editForOpen={false}
+				/>	
+			</div>
+		);
+	}
+}
+
+class ToggleableTimerForm extends React.Component {
+	render() {
+		if (this.props.isOpen) {
+			return (
+				<TimerForm />
+			)
+		} else {
+			return (
+				<a href="#"><AiOutlinePlus /></a>
+			)
+		}
 	}
 }
