95f583873613c850bd9004e6b3655e418b55241a
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-08-16 20:46:19 +0530

    feat/adding tailwind to the timer react project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class TimersDashboard extends React.Component {
+	render() {
+		return(
+			<div className="max-w-sm mx-auto mt-8 border border-black border-solid rounded-lg">
+				<h2 className="mx-auto mt-4 text-3xl max-w-max">Timers</h2>
+			</div>
+		)
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 import TimersDashboard from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-	<TimersDashboard />
+	<div classname=''>
+		<TimersDashboard />
+	</div>
 )
