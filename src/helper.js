d0416192bb91890d21116c90fa09d027f91972d0
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-08-17 08:55:06 +0530

    feat/helper function to convert seconds to readable format

---
+++ src/helper.js
@@ -0,0 +1,15 @@
+const convertToHr = (seconds) => {
+	let minutes=0;
+	let hours=0;
+	if (seconds > 60) {
+		minutes = Math.floor(seconds / 60);
+		seconds = seconds % 60;
+		if (minutes > 60) {
+			hours = Math.floor(minutes / 60);
+			minutes = minutes % 60;
+		}
+	}
+	return `${hours}:${minutes}:${seconds}`
+}
+
+console.log(convertToHr(184957));
